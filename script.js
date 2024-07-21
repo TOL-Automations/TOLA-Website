568fa9854f162c7ab1607cc3c549b62e29d8c8db
TOL-Automations <tol.automations@outlook.com>
2024-07-21 13:12:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.querySelector('.navbar');
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 0) {
+            navbar.classList.add('sticky');
+        } else {
+            navbar.classList.remove('sticky');
+        }
+    });
+
+    hamburger.addEventListener('click', function () {
+        navLinks.classList.toggle('show');
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    var word = 'Autopilot?',
+        part,
+        offset = 0,
+        forwards = true,
+        skip_count = 0,
+        skip_delay = 15,
+        speed = 100;
+  
+    function type() {
+      setInterval(function () {
+        if (forwards) {
+          if (offset >= word.length) {
+            ++skip_count;
+            if (skip_count == skip_delay) {
+              forwards = false;
+              skip_count = 0;
+            }
+          }
+        } else {
+          if (offset == 0) {
+            forwards = true;
+            offset = 0;
+          }
+        }
+        part = word.substr(0, offset);
+        if (skip_count == 0) {
+          if (forwards) {
+            offset++;
+          } else {
+            offset--;
+          }
+        }
+        document.querySelector('.typing-word').textContent = part;
+      }, speed);
+    }
+  
+    type();
+  });
+  
